8f84d958291b0ae71a2e216510bc3db75145aabb
Yun <chyywhcg@gmail.com>
2026-03-15 10:31:56 -0700

    Day 30 Try-except-else-finally

---
+++ day-30/main.py
@@ -0,0 +1,80 @@
+# FileNotFound
+# with open("a_file.txt") as file:
+#     file.read()
+
+# try:
+#     file = open("a_file.txt")
+#     a_dictionary = {"key": "value"}
+#     print(a_dictionary["sdfsdf"])
+# except FileNotFoundError:
+#     file = open("a_file.txt", "w")
+#     file.write("Something")
+# except KeyError as error_message:
+#     print(f"The key {error_message} does not exist.")
+# else:
+#     content = file.read()
+#     print(content)
+# finally:
+#     raise TypeError("THis is an error that i made up")
+
+# height = float(input("Height: "))
+# weight = int(input("Weight: "))
+#
+# if height > 3:
+#     raise ValueError("Human Height should not be over 3 meters."
+#                      "")
+# bmi = weight / height ** 2
+# print(bmi)
+
+
+# KeyError
+# a_dictionary = {"key": "value"}
+# value = a_dictionary["non_existent_key"]
+
+# IndexError
+# fruit_list = ["Apple", "Banana", "Pear"]
+# fruit = fruit_list[3]
+
+# TypeError
+# text = "abc"
+# print(text + 5)
+
+
+"""随课练习 20"""
+# fruits = ["Apple", "Pear", "Orange"]
+#
+# # Catch the exception and make sure the code runs without crashing.
+# def make_pie(index):
+#     try:
+#         fruit = fruits[index]
+#         print(fruit + " pie")
+#     except IndexError:
+#         print("Fruit pie")
+#
+# make_pie(4)
+
+"""随课练习 21"""
+facebook_posts = [
+    {'Likes': 21, 'Comments': 2},
+    {'Likes': 13, 'Comments': 2, 'Shares': 1},
+    {'Likes': 33, 'Comments': 8, 'Shares': 3},
+    {'Comments': 4, 'Shares': 2},
+    {'Comments': 1, 'Shares': 1},
+    {'Likes': 19, 'Comments': 3}
+]
+
+"""也可以用这个防止程序崩溃：
+post.get('Likes', 0)"""
+
+def count_likes(posts):
+    total_likes = 0
+    for post in posts:
+        try:
+            total_likes = total_likes + post['Likes']
+        except KeyError:
+            total_likes = total_likes + 0
+
+    # return total_likes
+    print(total_likes)
+
+count_likes(facebook_posts)
